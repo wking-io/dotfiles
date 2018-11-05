39086db5c6910a5cc54b5930081751bc8f50cbdd
Will King <contact@wking.io>
2018-11-05 08:31:05 -0600

    Changed to rsync to copy files so that we could ignore node_modules etc

--- src/os/migrate.sh
+++ src/os/migrate.sh
@@ -1,46 +1,49 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")" \
-    && . "utils.sh"
+. "$HOME/.dotfiles/src/os/utils.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-main() {
-
-    print_in_purple "\n • Update content\n\n"
+make_folders() {
 
     # make folders
     mkdir -p ~/migration/home
     mkdir -p ~/migration/Library/
     mkdir -p ~/migration/rootLibrary/Preferences/SystemConfiguration/
+    mkdir -p $1
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # move into migrate directory
-
-    cd ~/migration
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # backup some dotfiles likely not under source control
-
-    cp -Rp
-        ~/.gitconfig.local
-        ~/.ssh \
-        ~/.z   \
-            ~/migration/home
-
-    cp -Rp ~/Documents ~/migration
-    cp -Rp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration/rootLibrary/Preferences/SystemConfiguration/ # wifi
-    cp -Rp ~/Library/Fonts ~/migration/Library/ # my fonts
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # move from ~/migration => usb/migration-{mm/dd/yyyy}
-
-    # mv ~/migration $1
-    echo $1
 }
 
-echo $0
-main $0
+backup() {
+
+    rsync --archive ~/.ssh ~/migration/home
+    rsync --archive ~/.z ~/migration/home
+    rsync --archive ~/.gitconfig.local ~/migration/home
+    rsync --archive ~/sites ~/migration/home --exclude node_modules --exclude .DS_Store
+
+    rsync --archive /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration/rootLibrary/Preferences/SystemConfiguration/ # wifi
+    rsync --archive ~/Library/Fonts ~/migration/Library/ # my fonts
+
+}
+
+main() {
+
+    print_in_purple "\n • Migrate\n\n"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # make folders
+    execute "make_folders $1" "Making migration folder in home directory"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # backup files
+
+    execute "backup" "Backing up files"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # move from ~/migration => usb/migration-{mm-dd-yyyy}
+    execute "mv ~/migration $1" "Moving local migration to External HDD"
+}
+
+main $1
